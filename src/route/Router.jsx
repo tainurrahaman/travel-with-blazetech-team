4b6373868c36b0cfd653ac9e3cb0144f76f748fe
tainur <tainurrahaman31@gmail.com>
2024-12-22 19:18:20 +0600

    add necessary assets and connect with react router dom

---
+++ src/route/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h2>Hello world!</h2>,
+  },
+]);
+
+export default Router;
